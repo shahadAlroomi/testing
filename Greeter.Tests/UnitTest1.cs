0a6cd43dd277262432f842cb8727595627755196
shahadAlroomi <shahad.ro@icloud.com>
2025-11-18 13:26:27 +0100

    first commit

---
+++ Greeter.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿namespace Greeter.Tests;
+
+public class HelloWorlsTests
+{
+    [Fact]
+    public void GetGreetingReturHelloWorld()
+    {
+        //Arrnge 
+        
+        string expected= "Hello,Shahad!";
+
+        //Act 
+
+        string actual = Program.GetGreeting("Shahad");
+
+        //Assert 
+
+        Assert.Equal(expected,actual);
+
+    }
+}
---
+++ Greeter/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+public class Program
+{
+    public static void Main()
+    {
+        
+    }
+    
+    public static string GetGreeting(string name)
+    {
+        return"Hello,"+ name + "!";
+    }
+}
