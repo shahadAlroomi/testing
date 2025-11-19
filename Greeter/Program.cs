840c7974d26bf40c8cfc3d3cac6ae111dc3c9a6a
shahadAlroomi <shahad.ro@icloud.com>
2025-11-19 08:54:38 +0100

    TEST

---
+++ Greeter/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Greeter;
+using System;
+public class Program
+{
+    public static void Main()
+    {
+        
+    }
+    //const string EnglishGreeting ="Hello";
+
+
+    public static string GetGreeting(string name, string language)
+
+    {
+
+        return name + "," + language ;
+    }
+}
